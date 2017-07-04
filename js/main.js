068fb679fec606d1ae23a0e9770eb5982d837a23
rensramos <rensramos17@gmail.com>
2017-07-05 00:08:19 +0800

    Update modal

--- js/main.js
+++ js/main.js
@@ -10,3 +10,19 @@ $(".radioRegistration[name='registrationRadio']").click(function () {
     $(".registration .radioListValue").val(radioListValue);
 
 });
+
+
+$("#form-content").submit(function(){
+    $("#ss-submit").attr("disabled", true);
+    setTimeout(function(){
+        $("#ss-submit").attr("disabled", false);
+    }, 1000);
+});
+
+$("#hidden_iframe").on('load',function () {
+    $("#myModal").modal('show');
+    $("#form-content")[0].reset();
+    setTimeout(function(){
+        $("#myModal").modal('hide');
+    }, 2000);
+});
