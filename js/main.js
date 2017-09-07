ff9e585e7e98465b2ad9e373f8a2bdd2bf8d0cc3
rensramos <rensramos17@gmail.com>
2017-09-07 17:02:24 +0800

    Update modal

--- js/main.js
+++ js/main.js
@@ -29,9 +29,7 @@ $("#form-content").on("submit",function(){
     $("#hidden_iframe").on('load',function () {
         $("#myModal").modal('show');
         $("#form-content")[0].reset();
-        setTimeout(function(){
-            $("#myModal").modal('hide');
-        }, 2000);
+        
     });
 });
 
